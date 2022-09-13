d7eda337e91aa6daeed80043c894e657ce327889
code-Adrian <adrianbsuper@gmail.com>
2022-09-13 10:35:17 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,1 +1,1 @@
-const aCar = { owner : "Joe Bloggs", type : {make: 'Toyota',model:'Corolla',cc:'1.8'}, registration : {year: 201,county_code:'WD',number:1058} };
+const aCar = { owner : "Joe Bloggs",address : '3 Walkers Lane',previous_owners: [{name: "Pat Smith", address: "1",street:"Main Street"},{name: "Sheila Dwyer", address: "2",street:"High Street"}], type : {make: 'Toyota',model:'Corolla',cc:'1.8'}, registration : {year: 201,county_code:'WD',number:1058} };
